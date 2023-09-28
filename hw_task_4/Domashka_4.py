0af3f228661cccf5945305ef521116da31b96d26
Andy Shengals <ashengals@ya.ru>
2023-09-28 13:42:33 +0300

    last commit

--- hw_task_4/Domashka_4.py
+++ hw_task_4/Domashka_4.py
@@ -56,18 +56,27 @@ def fn(A, S, last=False):
     X= len(A)
     B = np.random.randint(1, 5, (S,X), dtype=int)
     C= A * B
-    D = np.array([], int)
+    D = np.array([])
     for i in C:
-        D = np.append(D, np.sum(i))
+        el=np.sum(i)
+        D = np.append(D, el)
+
+    if last == False:
+        D = np.sin(D)
+    else:
+        D = np.maximum(D,0)
     return D,B
+
+
 A= np.array([1,3,5,7,4])
 S=10
-A_1, Ran_1= fn(A,S)
+A_1, Ran_1= fn(A, S)
 print (A_1,'\n',Ran_1)
 print('')
-A_2, Ran_2 = fn(A_1,S)
+A_2, Ran_2 = fn(A_1, S,False)
 print (A_2,'\n',Ran_2)
 print('')
 S=5
-A_3, Ran_3 = fn(A_2,S, True)
-print (A_3,'\n',Ran_3)
+A_3, Ran_3 = fn(A_2, S, True)
+A_4 = A_3*100
+print (A_4,'\n',Ran_3)
