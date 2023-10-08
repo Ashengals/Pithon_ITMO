7c9a6d588a577f3463e37a12236d445c889ae64a
Andy Shengals <ashengals@ya.ru>
2023-10-08 20:18:21 +0300

    last commit

--- hw_task_4/Domashka_4.py
+++ hw_task_4/Domashka_4.py
@@ -1,54 +1,81 @@
 # Задание 1
 
 import numpy as np
-# a=np.random.random(10)*20
-# print(sorted(a))
-#
-# # Задание 2
-#
-# x=np.zeros((8,8))
-#
-# x[1::2,::2]=1
-# x[::2,1::2]=1
-# print(x)
-#
-# # Задание 3
-#
-# a=np.ones((8,4))
-# b=np.ones((4,2))
-# print(np.dot(a,b))
-#
-# # Задание 4
-# st=0
-# mas=np.array(range(10), float)
-# while st<10:
-#     ran = np.random.random()
-#     if ran!=0 or ran!=1:
-#         mas[st]=ran
-#         st=st+1
-#
-# print(mas)
-#
-#
-# # Задание 5
-#
-# def matrix(number):
-#     spis = list()
-#     for i in range(2, number):
-#         if number % i ==0:
-#             spis.append(i)
-#     if len(spis)==0:
-#         return print ("число простое")
-#     print(spis)
-#     for i in range(0,len(spis)):
-#         for j in range(0,len(spis)):
-#             mat=np.ones((spis[i],spis[j]))
-#             print(mat)
-#
-# matrix(6)
-#
+import re
+a=np.random.random(10)*20
+print(sorted(a))
+
+# Задание 2
+
+x=np.zeros((8,8))
+
+x[1::2,::2]=1
+x[::2,1::2]=1
+print(x)
+
+# Задание 3
+
+a=np.ones((8,4))
+b=np.ones((4,2))
+print(np.dot(a,b))
+
+# Задание 4
+st=0
+mas=np.array(range(10), float)
+while st<10:
+    ran = np.random.random()
+    if ran!=0 or ran!=1:
+        mas[st]=ran
+        st=st+1
+
+print(mas)
+
+
+# Задание 5
+
+def matrix(number):
+    spis = list()
+    for i in range(2, number):
+        if number % i ==0:
+            spis.append(i)
+    if len(spis)==0:
+        return print ("число простое")
+    print(spis)
+    for i in range(0,len(spis)):
+        for j in range(0,len(spis)):
+            mat=np.ones((spis[i],spis[j]))
+            print(mat)
+
+matrix(6)
+
 
 # Задание 6
+class Data_file:
+
+    def __init__(self, path):
+        self.path=path
+
+    def f_open(self):
+        global text
+
+        t=list()
+        with open(self.path) as f:
+            t.append(f.readlines())
+        text=sum(t,[])
+
+
+    def f_find(self, a):
+        new_t=list()
+        for i in text:
+            res=re.findall(a,i)
+            if res:
+                new_t.append(res)
+        return sum(new_t,[])
+
+t1=Data_file('text.txt')
+t1.f_open()
+print(t1.f_find('\d{2}'))
+print(t1.f_find('\d{3}'))
 
 # Задание 7
 
@@ -74,9 +101,9 @@ A_1, Ran_1= fn(A, S)
 print (A_1,'\n',Ran_1)
 print('')
 A_2, Ran_2 = fn(A_1, S,False)
-print (A_2,'\n',Ran_2)
-print('')
-S=5
-A_3, Ran_3 = fn(A_2, S, True)
-A_4 = A_3*100
-print (A_4,'\n',Ran_3)
+# print (A_2,'\n',Ran_2)
+# print('')
+# S=5
+# A_3, Ran_3 = fn(A_2, S, True)
+# A_4 = A_3*100
+# print (A_4,'\n',Ran_3)
